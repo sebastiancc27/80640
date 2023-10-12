d408a44ed2500e9754694255278fbbb887cd4f49
sebastiancc27 <sebasccastizo27@gmail.com>
2023-10-12 12:58:04 -0600

    practica08

---
+++ practica08/src/MiFieldSet.jsx
@@ -0,0 +1,18 @@
+
+function MiFieldSet(prompt){
+    const {titulo,txt1,txt2}=prompt;
+return (
+    <>
+    <fieldset>
+        <legend>{titulo}</legend>
+        <label htmlFor={txt1}>{txt1}:</label>
+        <input type="text" id={txt1}/>
+
+        <label htmlFor={txt2}>{txt2}:</label>
+        <input type="text" id={txt2}/>
+    </fieldset>
+    </>
+)
+}
+
+export default  MiFieldSet;
